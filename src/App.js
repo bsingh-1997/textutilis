d401d76547104dcf0427b0771f3e490bfd80a3b7
barinder <barindersingh1997@gmail.com>
2023-01-27 23:23:59 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './personel/header';
 import Middle from './personel/middle';
-import Middlecont from './personel/middlecont';
-import Middlecont2 from './personel/middlecont2';
 import { useState } from 'react';
 import Middleform from './personel/middleform';
 import Alert from './personel/alert';
-
-
 import React from 'react';
-import ReactDOM from 'react-dom'
 
-// import {Switch} from 'react-router-dom';
-// import {Route,Switch} from 'react-router';
 import {
   BrowserRouter,
   Route,
@@ -45,31 +37,22 @@ function App() {
       document.title = 'Text-utility'
     }
   }
+
   return (
     <>
       <Header name="Text-Utils" mode={mode} tgm={tgm} />
       <Alert alert={alert} />
 
       <BrowserRouter basename='/textutilis'>
-        {/* <Switch> */}
+
         <Routes>
-          {/* <Route path="/textutilis" element={<Middleform showalert={showalert} mode = {mode}/>}></Route> */}
-          {/* <Route exact path='/textutilis' element={<Header name="Text-Utils" mode={mode} tgm={tgm} />}/> */}
-          {/* <Route exact path='/about' element={<Alert alert={alert} />}/> */}
+
           <Route path='/textutilis' element={<Middleform showalert={showalert} mode={mode} />} />
-          <Route exact path="/abouft" element={<Middle />} />
+          <Route exact path="/about" element={<Middle />} />
+
         </Routes>
 
-        {/* </Switch> */}
       </BrowserRouter>
-
-
-      {/* <Middle 
-    body1="this is stuff for body 1"
-    body2="this is stuff for body two"
-    body3="this is stuff for body 3ree"
-    mode={mode}/>
-    <Middlecont mode = {mode}/> */}
 
     </>
   );
--- src/personel/middle.js
+++ src/personel/middle.js
@@ -1,87 +1,76 @@
 import React from 'react'
-import Middlecont from './middlecont';
-import Middleform from './middleform';
 import { NavLink } from 'react-router-dom';
 import './buttoncss.css';
 
 const Middle = (props) => {
   return (
     <>
-    <div className='container'  >
-    <div className='buttons1'>
-    <ul>
-    <li><NavLink to="/" className="c">Home</NavLink></li>
-    <li><NavLink to="/about" className="c">About</NavLink></li>
-    </ul>
-    </div>
-    <div className="accordion my-4" id="accordionExample" style={{backgroundColor:props.mode==='dark'?'white':'grey'}}>
-  <div className="card" style={{backgroundColor:props.mode==='dark'?'#343a40':'white'}}>
-    <div className="card-header" id="headingOne">
-      <h2 className="mb-0">
-        <button className="btn btn-link" type="button" style={{color:props.mode==='dark'?'white':'black'}} data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-          Introduction
-        </button>
-      </h2>
-    </div>
+      <div className='container'  >
+        <div className='buttons1'>
+          <ul>
+            <li><NavLink to="/" className="c">Home</NavLink></li>
+            <li><NavLink to="/about" className="c">About</NavLink></li>
+          </ul>
+        </div>
+        <div className="accordion my-4" id="accordionExample" style={{ backgroundColor: props.mode === 'dark' ? 'white' : 'grey' }}>
+          <div className="card" style={{ backgroundColor: props.mode === 'dark' ? '#343a40' : 'white' }}>
+            <div className="card-header" id="headingOne">
+              <h2 className="mb-0">
+                <button className="btn btn-link" type="button" style={{ color: props.mode === 'dark' ? 'white' : 'black' }} data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                  Introduction
+                </button>
+              </h2>
+            </div>
 
-    <div id="collapseOne" className="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
-      <div className="card-body" style={{color:props.mode==='dark'?'white':'black'}}>
-        {props.body1}
+            <div id="collapseOne" className="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+              <div className="card-body" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
+                {props.body1}
+              </div>
+            </div>
+          </div>
+          <div className="card" style={{ backgroundColor: props.mode === 'dark' ? '#343a40' : 'white' }}>
+            <div className="card-header" id="headingTwo">
+              <h2 className="mb-0">
+                <button className="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" style={{ color: props.mode === 'dark' ? 'white' : 'black' }} aria-expanded="false" aria-controls="collapseTwo">
+                  available items
+                </button>
+              </h2>
+            </div>
+            <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+              <div className="card-body" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
+
+                {props.body2}
+                <img src='https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg'></img>
+                
+              </div>
+            </div>
+          </div>
+          <div className="card" style={{ backgroundColor: props.mode === 'dark' ? '#343a40' : 'white' }}>
+            <div className="card-header" id="headingThree">
+              <h2 className="mb-0">
+                <button style={{ color: props.mode === 'dark' ? 'white' : 'black' }} className="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree" >
+                  Collapsible Group Item #3
+                </button>
+              </h2>
+            </div>
+            <div id="collapseThree" className="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+              <div className="card-body">
+                {props.body3}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-  <div className="card" style={{backgroundColor:props.mode==='dark'?'#343a40':'white'}}>
-    <div className="card-header" id="headingTwo">
-      <h2 className="mb-0">
-        <button className="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" style={{color:props.mode==='dark'?'white':'black'}} aria-expanded="false" aria-controls="collapseTwo">
-          available items
-        </button>
-      </h2>
-    </div>
-    <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-      <div className="card-body" style={{color:props.mode==='dark'?'white':'black'}}>
-
-        {props.body2}
-        <img src='https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg'></img>
-
-
-
+      <div className='container'>
+        <div className="jumbotron" style={{ backgroundColor: props.mode === 'dark' ? '#343a40' : 'white' }}>
+          <h1 className="display-4" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>Hello, world!</h1>
+          <p className="lead" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+          <hr className="my-4" style={{ color: props.mode === 'dark' ? 'white' : 'black' }} />
+          <p style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+          <a className="btn btn-primary btn-lg" href="#" role="button" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>Learn more</a>
+          <NavLink to="/">home</NavLink>
+        </div>
       </div>
-    </div>
-  </div>
-  <div className="card" style={{backgroundColor:props.mode==='dark'?'#343a40':'white'}}>
-    <div className="card-header" id="headingThree">
-      <h2 className="mb-0">
-        <button style={{color:props.mode==='dark'?'white':'black'}} className="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree" >
-          Collapsible Group Item #3
-        </button>
-      </h2>
-    </div>
-    <div id="collapseThree" className="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
-      <div className="card-body">
-        {props.body3}
-      </div>
-    </div>
-  </div>
-</div>
-    </div>
-
-
-    <div className='container'>
-
-    <div className="jumbotron" style={{backgroundColor:props.mode==='dark'?'#343a40':'white'}}>
-  <h1 className="display-4" style={{color:props.mode==='dark'?'white':'black'}}>Hello, world!</h1>
-  <p className="lead" style={{color:props.mode==='dark'?'white':'black'}}>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-  <hr className="my-4" style={{color:props.mode==='dark'?'white':'black'}}/>
-  <p style={{color:props.mode==='dark'?'white':'black'}}>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-  <a className="btn btn-primary btn-lg" href="#" role="button" style={{color:props.mode==='dark'?'white':'black'}}>Learn more</a>
-    <NavLink to="/">home</NavLink>
-</div>
-
-
-
-    </div>
-
     </>
   )
 }
