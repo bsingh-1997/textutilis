d776efb7a50b9367f7a28bb22b45caf8d98cf13e
barinder <barindersingh1997@gmail.com>
2023-01-27 18:06:19 +0530

    pathh1

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ function App() {
           {/* <Route path="/textutilis" element={<Middleform showalert={showalert} mode = {mode}/>}></Route> */}
           {/* <Route exact path='/textutilis' element={<Header name="Text-Utils" mode={mode} tgm={tgm} />}/> */}
           {/* <Route exact path='/about' element={<Alert alert={alert} />}/> */}
-          <Route exact path='/textutilis' element={<Middleform showalert={showalert} mode={mode} />} />
+          <Route path='/textutilis' element={<Middleform showalert={showalert} mode={mode} />} />
           <Route exact path="/abouft" element={<Middle />} />
         </Routes>
 
